e9fdbbb43efd4df3d75033c21dde2a2154dc4829
GHOSTY <84158684+VEXORjs@users.noreply.github.com>
2026-03-31 20:41:51 +0200

    Add files via upload

---
+++ htu21d_i2c_hal.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022, Mezael Docoy
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "htu21d_i2c_hal.h"
+#include "i2c.h"
+
+int16_t htu21d_i2c_hal_init()
+{
+    I2CInit(I2CMASTER, 0);
+    return 0;
+}
+
+int16_t htu21d_i2c_hal_write(uint8_t addr, uint8_t *data, uint16_t len)
+{
+    I2CWrite(addr << 1, data, len);
+    return 0;
+}
+
+int16_t htu21d_i2c_hal_read(uint8_t addr, uint8_t *reg, uint8_t *data, uint16_t len)
+{
+    // 1. Wyślij komendę (np. trigger pomiaru)
+    I2CWrite(addr << 1, reg, 1);
+
+    // 2. Odczytaj dane
+    I2CRead(addr << 1, data, len);
+
+    return 0;
+}
+
+void htu21d_i2c_hal_ms_delay(uint32_t ms)
+{
+    for (volatile uint32_t i = 0; i < ms * 5000; i++);
+}
