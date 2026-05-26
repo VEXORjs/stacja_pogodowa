f62c4860a872caee1313899898cd3eae859f2dba
GHOSTY <84158684+VEXORjs@users.noreply.github.com>
2026-05-26 17:14:26 +0200

    Refactor main.c to add air quality and sound features

--- main.c
+++ main.c
@@ -28,17 +28,71 @@
 #include "temp.h"
 //#include "acc.h"
 #include "ff.h"
+#include "hmc5883l_raw.h"
 
 #define HTU21D_I2C_ADDRESS (0x40<<1)
 #define CMD_TRIG_HUMD_NOHOLD 0xF5
 
 #define BMP180_ADDRESS (0x77<<1)
 #define OSS 3
+#define SGP40_ADDR 0x59
+#define SGP40_CMD_MSB 0x26
+#define SGP40_CMD_LSB 0x0F
+
+#define HMC_ADDR (0x1E << 1)
 
 static uint32_t msTicks = 0;
 static uint8_t buf[20];
 
+#define P1_2_HIGH() (LPC_GPIO1->DATA |= (0x1<<2))
+#define P1_2_LOW()  (LPC_GPIO1->DATA &= ~(0x1<<2))
 
+#define IM_G4  3189   // G4  392Hz
+#define IM_E4  3030   // E4  330Hz  (tu: Eb4 ~311Hz, ale E4 brzmi ok)
+#define IM_C4  3816   // C4  262Hz
+#define IM_D4  2840   // D4  352Hz  (Db4)
+#define IM_A3  4545   // A3  220Hz
+#define IM_F4  2865   // F4  349Hz
+#define IM_BB3 3584   // Bb3 279Hz
+#define IM_AB3 3795   // Ab3 263Hz
+#define IM_REST 0
+
+typedef struct {
+    uint32_t freq;   // okres µs
+    uint32_t dur;    // czas ms
+} ImNote;
+
+static const ImNote imperial[] = {
+    // "DUM DUM DUM"
+    {IM_G4, 3000}, {IM_REST, 600},
+    {IM_G4, 3000}, {IM_REST, 600},
+    {IM_G4, 3000}, {IM_REST, 600},
+
+    // "DUM di-dum"
+    {IM_E4, 2240}, {IM_REST, 300},
+    {IM_BB3, 760}, {IM_REST, 300},
+    {IM_G4, 3600}, {IM_REST, 900},
+
+    // "DUM di-dum"
+    {IM_E4, 2240}, {IM_REST, 300},
+    {IM_BB3, 760}, {IM_REST, 300},
+    {IM_G4, 5400}, {IM_REST, 1200},
+
+    // drugi motyw "DUM DUM DUM"
+    {IM_D4, 3000}, {IM_REST, 600},
+    {IM_D4, 3000}, {IM_REST, 600},
+    {IM_D4, 3000}, {IM_REST, 600},
+
+    // "DUM di-dum"
+    {IM_F4, 2240}, {IM_REST, 300},
+    {IM_BB3, 760}, {IM_REST, 300},
+    {IM_AB3, 3600},{IM_REST, 900},
+
+    // "DUM di-dum"
+    {IM_E4, 2240}, {IM_REST, 300},
+    {IM_BB3, 760}, {IM_REST, 300},
+    {IM_G4, 5400}, {IM_REST, 1200},
+};
 
 static void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
 {
@@ -247,24 +301,125 @@ int32_t bmp180_read_pressure_pa(void)
     return bmp180_get_pressure_pa_fixed(ut, up);
 }
 
-
 void SysTick_Handler(void) {
     msTicks++;
 }
 
+static void playNote(uint32_t note, uint32_t durationMs) {
+    uint32_t t = 0;
+    if (note > 0) {
+    	SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
+        while (t < (durationMs * 1000)) {
+            P1_2_HIGH();
+            // prosta pętla zamiast delay32Us
+            volatile uint32_t i;
+            for(i = 0; i < note/4; i++) __NOP();
+
+            P1_2_LOW();
+            for(i = 0; i < note/4; i++) __NOP();
+
+            t += note;
+        }
+        SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
+    } else {
+        delay32Ms(0, durationMs);
+    }
+}
+
+static void playImperialMarch(void) {
+    uint32_t count = sizeof(imperial) / sizeof(ImNote);
+    for (uint32_t i = 0; i < count; i++) {
+        playNote(imperial[i].freq, imperial[i].dur);
+    }
+}
+
+uint16_t read_airquality_raw(){
+	uint8_t cmd[5] = {SGP40_CMD_MSB, SGP40_CMD_LSB, 0x80, 0x00, 0xA2};
+	uint8_t buf[3];
+
+	I2CWrite(SGP40_ADDR, cmd, 5);
+	delay32Ms(0, 32);
+	I2CRead(SGP40_ADDR, buf, 3);
+
+	return ((uint16_t)buf[0] << 8) | buf[1];
+}
+
+
+
+//static void init_pwm(void)
+//{
+//  LPC_PWM1->MR0 = 1000; // okres pwm
+//  LPC_PWM1->LER |= (1U << 0U); // zatwierdzenie MR0
+//  // rejestry MR są 32-bitowe
+//
+//  // bit 0 - wlaczenie glownego licznika i prescalera
+//  // bit 3 - pwm enable
+//  LPC_PWM1->TCR |= (1U << 0U) | (1U << 3U);
+//
+//  PINSEL_CFG_Type PinCfg;
+//  PinCfg.Portnum = 2;
+//  PinCfg.Pinmode = 0;
+//  PinCfg.Funcnum = 1;
+//  PinCfg.OpenDrain = 0;
+//
+//  // PIO1_9
+//  PinCfg.Pinnum = 0;
+//  PINSEL_ConfigPin(&PinCfg);
+//  LPC_PWM1->MR1 = 500; // 50%
+//  LPC_PWM1->LER |= (1U << 1U); // zatwierdzenie rejestru MR1
+//  LPC_PWM1->PCR |= (1U << (9U + 0U)); // aktywacja wyjscia sygnalu dla kanalu 2
+//
+//  // PIO2_3
+//  PinCfg.Pinnum = 3;
+//  PINSEL_ConfigPin(&PinCfg);
+//  LPC_PWM1->MR4 = 500; // 50%
+//  LPC_PWM1->LER |= (1U << 4U); // zatwierdzenie MR4
+//  LPC_PWM1->PCR |= (1U << (9U + 3U));
+//}
+//
+//static void my_set_pwm_value(int channel, int value)
+//{
+//  if (channel == 1)
+//  {
+//    LPC_PWM1->MR1 = value;
+//    LPC_PWM1->LER |= (1U << 1U);
+//  }
+//  else if (channel == 2)
+//  {
+//    LPC_PWM1->MR4 = value;
+//    LPC_PWM1->LER |= (1U << 4U);
+//  }
+//}
+//
 
 int main (void)
 {
+	LPC_IOCON->JTAG_nTRST_PIO1_2 = (LPC_IOCON->JTAG_nTRST_PIO1_2 & ~0x7) | 0x01;
+	GPIOSetDir(PORT1, 2, 1);
+
+	// wzmacniacz LM4811 - wybudź
+	GPIOSetDir(PORT3, 0, 1);  // clk
+	GPIOSetDir(PORT3, 1, 1);  // up/dn
+	GPIOSetDir(PORT3, 2, 1);  // shutdn
+	GPIOSetValue(PORT3, 0, 0);
+	GPIOSetValue(PORT3, 1, 0);
+	GPIOSetValue(PORT3, 2, 0);
+
+
 	int32_t ticks = 0;
 
+	uint16_t raw_air_quality;
     int32_t t = 0;
     int32_t tempK = 0;
     int32_t humidity = 0;
     int32_t light = 0;
     int32_t pressure = 0;
+    int16_t mag[3];
+    int16_t x_gauss, y_gauss, z_gauss;
+    int32_t b_raw;
+    int32_t b;
 
     GPIOInit();
-    init_timer32(0, 10);
 
     UARTInit(115200);
     UARTSendString((uint8_t*)"OLED - Peripherals\r\n");
@@ -286,6 +441,13 @@ int main (void)
     temp_init (&getTicks);
 
     bmp180_read_calibration();
+
+//    init_pwm();
+//
+//    my_set_pwm_value(1, 500);
+
+    HMC_Init();
+    delay32Ms(0, 10);
 
     /* setup sys Tick. Elapsed time is e.g. needed by temperature sensor */
     SysTick_Config(SystemCoreClock / 1000);
@@ -313,18 +475,23 @@ int main (void)
     oled_clearScreen(OLED_COLOR_BLACK);
 
     oled_putString(1,1,  (uint8_t*)"Temp(C): ", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-    oled_putString(1,10,  (uint8_t*)"Temp(K): ", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    oled_putString(1,10,  (uint8_t*)"Raw VOC: ", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
     oled_putString(1,20,  (uint8_t*)"Humi(%): ", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
     oled_putString(1,30, (uint8_t*)"Lux(lx): ", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
     oled_putString(1,40, (uint8_t*)"Press: ", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 
-    while(1) {
+    uint8_t alarm_played = 0;
 
+    while(1) {
+    	UARTSendString((uint8_t*)"ping\r\n");
+    	delay32Ms(0, 1000);
     	/* Light sensor*/
     	light = light_read();
 
         /* Temperature */
         t = temp_read();
+
+        raw_air_quality = read_airquality_raw();
 
         /* Humidity */
         humidity = read_humidity();
@@ -332,22 +499,46 @@ int main (void)
         /* Pressure */
         pressure = bmp180_read_pressure_pa();
 
+        HMC_ReadXYZ(mag);
+
+        int32_t mx = mag[0];
+        int32_t my = mag[1];
+        int32_t mz = mag[2];
+
+        b_raw = (int32_t)sqrt((double)(mx*mx + my*my + mz*mz));
+        b = (b_raw * 1000) / 1090;
+
         /* output values to OLED display */
         sprintf(buf,"%2d.%dC",t/10, t%10 );
         oled_putString((1+9*6),1, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 
         tempK = (t/10) + 273;
-        sprintf(buf,"%3dK", tempK);
+        //sprintf(buf,"%3dK", tempK);
+        //oled_putString((1 + 9*6), 10, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+//        sprintf(buf, "%d %d %d", mag[0], mag[1], mag[2]);
+//        oled_putString(10, 10, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+        sprintf(buf,"%d", raw_air_quality);
         oled_putString((1 + 9*6), 10, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 
         sprintf(buf, "%d.%d%%", humidity/10, humidity%10);
         oled_putString((1 + 9*6), 20, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+        if (light < 200 && !alarm_played) {
+        	alarm_played = 1;
+        	playImperialMarch();
+        } else {
+        	alarm_played = 0;
+        }
 
         sprintf(buf, "%d", light);
         oled_putString((1 + 9*6), 30, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 
         sprintf(buf, "%d.%d", pressure/100, pressure%100);
         oled_putString((1 + 9*6), 40, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+        sprintf(buf, "%d.%03d", b/1000, b%1000);
+        oled_putString((1 + 9*6), 50, buf, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 
         /* delay */
         delay32Ms(0, 15);
