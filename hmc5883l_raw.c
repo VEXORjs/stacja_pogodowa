9505c67a4d9b9db40b34a3bc5cfd8914a6722cc0
GHOSTY <84158684+VEXORjs@users.noreply.github.com>
2026-05-29 17:53:09 +0200

    Update HMC5883L configuration and reading logic

--- hmc5883l_raw.c
+++ hmc5883l_raw.c
@@ -10,11 +10,19 @@
 #include "uart.h"
 #include "timer32.h"
 
-#define HMC_ADDR (0x0D << 1)
+//#define HMC_ADDR (0x0D << 1)
+#define HMC_ADDR (0x1e << 1)
+
 
 void HMC_Init(void)
 {
-	HMC_WriteReg(0x09, 0x01);
+	 // Config A: 8-average, 15 Hz, normal measurement
+	HMC_WriteReg(0x00, 0x70);
+
+	    // Config B: gain
+    HMC_WriteReg(0x01, 0x20);
+
+	HMC_WriteReg(0x02, 0x00);
 }
 
 void HMC_WriteReg(uint8_t reg, uint8_t data)
@@ -28,14 +36,17 @@ void HMC_WriteReg(uint8_t reg, uint8_t data)
 
 void HMC_ReadXYZ(int16_t *mag)
 {
-    uint8_t reg = 0x00;
+    uint8_t reg = 0x03;
     uint8_t buf[6];
 
     I2CWrite(HMC_ADDR, &reg, 1);
+
+    delay32Ms(0, 1);
+
     I2CRead(HMC_ADDR, buf, 6);
 
 
-    mag[0] = (int16_t)((buf[0] << 8) | buf[1]); // X
-    mag[2] = (int16_t)((buf[2] << 8) | buf[3]); // Z
-    mag[1] = (int16_t)((buf[4] << 8) | buf[5]);
+    mag[0] = (int16_t)((buf[1] << 8) | buf[0]); // X
+    mag[1] = (int16_t)((buf[3] << 8) | buf[2]); // Y
+    mag[2] = (int16_t)((buf[5] << 8) | buf[4]); // Z
 }
